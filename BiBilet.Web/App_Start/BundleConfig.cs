0929af90cbb3adf9250b25af1115b02f302e1ac2
Eren Pinaz <pinaz.eren@gmail.com>
2016-03-23 00:31:18 +0200

    Bootstrap datetimepicker implemented

--- BiBilet.Web/App_Start/BundleConfig.cs
+++ BiBilet.Web/App_Start/BundleConfig.cs
@@ -8,17 +8,19 @@ namespace BiBilet.Web
         {
             bundles.Add(new StyleBundle("~/assets/css/app")
                 .Include("~/assets/css/site.css")
+                .Include("~/assets/css/bootstrap-datetimepicker.css")
                 .Include("~/assets/css/bootstrap-table.css"));
 
             bundles.Add(new ScriptBundle("~/assets/js/app")
                 .Include("~/assets/js/jquery-{version}.js")
                 .Include("~/assets/js/jquery.validate*")
+                .Include("~/assets/js/moment.js")
                 .Include("~/assets/js/bootstrap.js")
+                .Include("~/assets/js/bootstrap-datetimepicker.js")
                 .Include("~/assets/js/bootstrap-table/bootstrap-table.js")
                 .Include("~/assets/js/bootstrap-table/locale/bootstrap-table-tr-TR.js")
                 .Include("~/assets/js/plupload/moxie.js")
                 .Include("~/assets/js/plupload/plupload.dev.js")
-                .Include("~/assets/js/moment.js")
                 .Include("~/assets/js/site.js"));
 
             bundles.Add(new ScriptBundle("~/assets/js/tinymce/tinymce.min.js")
