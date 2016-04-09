15053e3ded8ad3de2107cc5c47be43a227e0a797
Eren Pinaz <pinaz.eren@gmail.com>
2016-04-09 15:22:25 +0300

    Unwanted headers removed

---
+++ BiBilet.Web/HttpModules/ResponseHeaderModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+
+namespace BiBilet.Web.HttpModules
+{
+    public class ResponseHeaderModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("Server");
+            HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+            HttpContext.Current.Response.Headers.Remove("X-AspNetMvc-Version");
+        }
+    }
+}
